99fd48a9539cfb374ccdb941b974a001d9f9c2c0
KOUSHIK <54579643+koushhik@users.noreply.github.com>
2020-07-29 19:38:07 +0530

    Create Arrayindexoutofbounds.java

---
+++ Arrayindexoutofbounds.java
@@ -0,0 +1,11 @@
+public class ArrayIndexOutOfBounds {
+
+	public static void main(String[] args) {
+		int a[]= {1,2,3,4,5};
+		int i;
+		for(i=0;i<=a.length;i++)
+			System.out.println(a[i]);
+		
+	}
+
+}
